5173e4e97592200329986e810c472e1254570000
sivaparthi <sivaparthi1989@gmail.com>
2025-06-24 09:46:24 +0000

    added allowedhosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-73j!d&*h(l!^5_%(-o@+$p2=ksgwfjc0w)s#0y515=cb@!tjkr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
